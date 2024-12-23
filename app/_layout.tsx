1347fc1e9234f1f3d392db773a1036e929ab07a2
ozankenangungor <s220204044@ankarabilim.edu.tr>
2024-12-23 10:58:21 +0300

    a

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,11 @@
 import '../global.css';
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView className="flex-1">
+      <Stack />
+    </GestureHandlerRootView>
+  );
 }
